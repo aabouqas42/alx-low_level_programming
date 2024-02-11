b2e7ca15b9911ffe58d56e8d4e1d348075a1245e
aabouqas <Aissam520@gmail.com>
2024-02-11 14:14:58 +0100

    done

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,11 +3,9 @@
 
 /**
  * ABS - macro function
- * 
  * Description:
  * macro thats calculate the absolut value of the geving number
  * @x: the number to calculate
- * 
  * Return:
  * return absolut value of x
 */
